c02f40b150b7ca34a54378b215be865f4c95dba0
Sosthène MAHUNGU <sosthene.mahungu@outlook.com>
2025-01-10 13:58:17 +0100

    [TECH] Initialization of the project.

---
+++ src/main/java/com/smahungu/kata/sg/KataSgApplication.java
@@ -0,0 +1,13 @@
+package com.smahungu.kata.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataSgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smahungu/kata/sg/KataSgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smahungu.kata.sg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
