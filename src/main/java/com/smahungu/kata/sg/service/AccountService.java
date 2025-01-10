5eb2fec4ad9750e629c7eada2addcff5d656d762
Sosthène MAHUNGU <sosthene.mahungu@outlook.com>
2025-01-10 16:45:55 +0100

    [US1] Add of history for amount deposit.

--- src/main/java/com/smahungu/kata/sg/service/AccountService.java
+++ src/main/java/com/smahungu/kata/sg/service/AccountService.java
@@ -1,13 +1,19 @@
 package com.smahungu.kata.sg.service;
 
+import com.smahungu.kata.sg.enums.OperationType;
 import com.smahungu.kata.sg.exception.NoPositiveAmountException;
 import com.smahungu.kata.sg.model.AccountModel;
+import com.smahungu.kata.sg.model.OperationModel;
+
+import java.time.LocalDateTime;
 
 public class AccountService {
 
     public AccountModel deposit(AccountModel accountModel, float amount) {
         validateDepositAmount(amount);
         accountModel.setBalance(accountModel.getBalance() + amount);
+        historize(accountModel, new OperationModel((long)accountModel.getOperations().size(), OperationType.DEPOSIT, amount, amount + accountModel.getBalance(),
+                LocalDateTime.now()));
         return accountModel;
     }
 
@@ -16,6 +22,11 @@ public class AccountService {
             throw new NoPositiveAmountException("The deposit amount must be greater than zero.");
         }
     }
+
+    public void historize(AccountModel accountModel, OperationModel operationModel) {
+        accountModel.getOperations().add(operationModel);
+    }
+
 
 
 }
